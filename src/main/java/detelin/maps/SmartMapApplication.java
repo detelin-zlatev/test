fe89b957c3b82f457724ed12f13c6a7211f23399
Detelin Zlatev <detelin.zlatev@cayetanogaming.com>
2016-04-08 16:06:13 +0300

    initial commit

---
+++ src/main/java/detelin/maps/SmartMapApplication.java
@@ -0,0 +1,12 @@
+package detelin.maps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMapApplication.class, args);
+	}
+}
